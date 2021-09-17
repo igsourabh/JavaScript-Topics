524f0857659d6e679d946c67064d1a6e79db973d
IGSourabh <90496971+IGSourabh@users.noreply.github.com>
2021-09-17 13:21:30 +0530

    Create Creating,Removing&ReplacinElements.js

---
+++ Creating,Removing&ReplacinElements.js
@@ -0,0 +1,30 @@
+// insert element in id
+
+let ss=document.createElement("li")
+
+ss.className=("dj")
+ss.id=("007")
+ss.setAttribute("type","list")
+ss.innerText=("Pythons")
+
+let ne=document.getElementsByClassName("no")[2]
+ne.appendChild(ss)
+console.log(ne)
+
+// insert element in id
+
+let ss=document.createElement("li")
+
+ss.className=("dj")
+ss.id=("007")
+ss.setAttribute("type","list")
+ss.innerText=("Pythons")
+
+let ne=document.getElementById("myfirst")
+ne.appendChild(ss)
+console.log(ne)
+
+// same in querryselector
+
+
+
