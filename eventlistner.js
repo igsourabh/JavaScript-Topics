eddf7dd4d20de200452af4d9c81cfa3b78f4632c
IGSourabh <90496971+IGSourabh@users.noreply.github.com>
2021-09-21 11:54:41 +0530

    Create eventlistner.js

---
+++ eventlistner.js
@@ -0,0 +1,14 @@
+// You have been clicked
+
+let ele=document.getElementById("heading").addEventListener("click",
+function() {
+  console.log("you have clicked");
+  location.href ="//www.codewithharry.com"
+});
+
+
+// After clicked redirect on another page
+let ele=document.getElementById("heading").addEventListener("click",
+function() {
+  console.log("you have clicked");
+});
