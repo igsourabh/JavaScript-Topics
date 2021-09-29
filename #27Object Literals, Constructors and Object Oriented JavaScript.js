94b4201bcf65ecc147374b34dd5f6b9d3a219bd4
IGSourabh <90496971+IGSourabh@users.noreply.github.com>
2021-09-29 19:24:19 +0530

    Update #27Object Literals, Constructors and Object Oriented JavaScript.js

--- #27Object Literals, Constructors and Object Oriented JavaScript.js
+++ #27Object Literals, Constructors and Object Oriented JavaScript.js
@@ -11,3 +11,19 @@ function genralCar(givenName, givenSpeed, givenEngien) {
 }
 let car = new genralCar('Mustang GT',180,"v8")
 console.log(car)
+
+// analyze function compaer car
+function genralCar(givenName, givenSpeed, givenEngien) {
+    this.name=givenName
+    this.speed=givenSpeed
+    this.engine=givenEngien
+    this.run= function run() {
+        console.log(`${this.name} is running`)
+    }
+    this.analyze=function() {
+     console.log(`this car is faster then ${500-this.speed}km/H  meredese`)
+        
+    }
+}
+car = new genralCar('Mustang GT',280,"v8")
+console.log(car)
