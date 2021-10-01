a0c65e8282aec902fbb3b59ed8ac859c4a8db690
IGSourabh <90496971+IGSourabh@users.noreply.github.com>
2021-10-01 20:06:28 +0530

    Update #27Object Literals, Constructors and Object Oriented JavaScript.js

--- #27Object Literals, Constructors and Object Oriented JavaScript.js
+++ #27Object Literals, Constructors and Object Oriented JavaScript.js
@@ -27,3 +27,35 @@ function genralCar(givenName, givenSpeed, givenEngien) {
 }
 car = new genralCar('Mustang GT',280,"v8")
 console.log(car)
+
+
+
+// ruff program set car speed name nd engien using setName
+function givenCar(GivenName, GivenEnigen,GivenSpeed) {
+    this.name=GivenName
+    this.engien=GivenEnigen
+    this.speed=GivenSpeed
+    this.run=function run() {
+        console.log(`${this.name} is running at ${this.speed}`)
+    }
+    
+givenCar.prototype.setName=function(newName) {
+ this.name=newName
+
+}
+
+givenCar.prototype.setName2=function(newEngien) {
+this.engien=newEngien
+   }
+   givenCar.prototype.setName3=function(newSpeed) {
+    this.speed=newSpeed   
+   }
+givenCar.prototype.getName=function() {
+        return this.name     
+   }
+     this.analyze=function() {
+        console.log(`this is  ${this.speed-180}Km/H faster then audi r8 `)
+    }
+}
+let car=new givenCar("Mustang","v8",240)
+console.log(car)
