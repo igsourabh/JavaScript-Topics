76282431ad55465b7558fef226e4ae7a94c79d86
IGSourabh <90496971+IGSourabh@users.noreply.github.com>
2021-10-03 11:12:33 +0530

    Create #31ES6 Classes and Inheritance.js

---
+++ #31ES6 Classes and Inheritance.js
@@ -0,0 +1,20 @@
+
+class Employee {
+constructor(GivenName, GivenExperience, GivenLanguage){
+   this.name=GivenName
+   this.experience=GivenExperience
+   this.language=GivenLanguage
+}    
+slogan(){
+   return `${this.name} is the best employ`
+}
+
+joining(){
+
+return ` ${this.name}Join this company   ${2021- this.experience} `
+
+}
+}
+harry = new Employee('Harry',20,'Pythone')
+console.log(harry)
+
