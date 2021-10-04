4d6eda5e2dbf342b51eaab00d2f4cb9a23498a53
IGSourabh <90496971+IGSourabh@users.noreply.github.com>
2021-10-04 19:13:07 +0530

    Update #31ES6 Classes and Inheritance.js

--- #31ES6 Classes and Inheritance.js
+++ #31ES6 Classes and Inheritance.js
@@ -18,3 +18,60 @@ return ` ${this.name}Join this company   ${2021- this.experience} `
 harry = new Employee('Harry',20,'Pythone')
 console.log(harry)
 
+// inheritance with class You have not need to edfiend every loggic in every class cunstructor
+
+class Employee {
+    constructor(GivenName, GivenExperience, GivenLanguage) {
+        this.name = GivenName
+        this.experience = GivenExperience
+        this.language = GivenLanguage
+    }
+    slogan() {
+
+        return `${this.name} is the best employ`
+    }
+
+
+    joining() {
+
+        return 2021 - this.experience
+
+    }
+
+    static add(a, b) {
+
+        return a + b;
+    }
+
+}
+
+harry = new Employee('Harry', 20, 'Pythone')
+// console.log(harry)
+
+class Programer extends Employee {
+    constructor(GivenName, GivenExperience, GivenLanguage, GivenGithub) {
+        super(GivenName, GivenExperience, GivenLanguage)
+        this.github = GivenGithub
+    }
+
+    favouritelanguage() {
+        if (this.language == "python") {
+            return "python"
+
+        }
+        else {
+            return "go"
+        }
+
+
+    }
+    static add(a, b) {
+        return a + b; }
+}
+rohan = new Programer('rohan', 3, 'flutter', 'rohan420')
+
+// console.log(rohan)
+console.log(rohan.favouritelanguage())
+
+
+
