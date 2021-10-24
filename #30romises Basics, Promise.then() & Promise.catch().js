4eb2208e8303b6da34c7f75249c8af58ae2f548b
IGSourabh <90496971+IGSourabh@users.noreply.github.com>
2021-10-24 11:47:54 +0530

    Update #30romises Basics, Promise.then() & Promise.catch().js

--- #30romises Basics, Promise.then() & Promise.catch().js
+++ #30romises Basics, Promise.then() & Promise.catch().js
@@ -23,3 +23,53 @@ console.log("thanks")
     console.log("not been resolved")
     
 })
+
+// promiss using call back 
+
+
+
+let students = [
+        {name: "harry", subject: "JavaScript"},
+        {name: "Rohan", subject: "Machine Learning"}
+     ]
+
+
+
+     function enrollStudent(student) {
+         return new Promise(function(resolve,reject) {
+             
+              setTimeout(() => {
+                   students.push(student) 
+                  console.log("student has been enrolled")
+                  const err=true
+                  if (!err) {
+                      resolve()
+                  }
+
+                  else{
+                reject()
+                  }
+                }, 5000);
+           
+    })
+         
+     }
+ 
+     function getstudents() {
+         setTimeout(() => {
+             let str="";
+            students.forEach(function(student) {
+                str+= `<li>${student.name}</li>`
+                
+            });
+            document.getElementById("students").innerHTML=str;
+            console.log("student has benn fetched")
+            }, 2000);
+         
+     }
+         let newstudent = {name: "Sourabh", subject: "Learning"}    
+
+enrollStudent(newstudent).then(getstudents).catch(function() {console.log("some error occur")
+    
+})
+// getstudents()
