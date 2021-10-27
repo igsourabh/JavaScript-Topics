36187e9c5a97f92afc5fe9587bab320e78209734
IGSourabh <90496971+IGSourabh@users.noreply.github.com>
2021-10-27 13:23:46 +0530

    Update #39pomises Basics, Promise.then() & Promise.catch().js

--- #39pomises Basics, Promise.then() & Promise.catch().js
+++ #39pomises Basics, Promise.then() & Promise.catch().js
@@ -28,48 +28,51 @@ console.log("thanks")
 
 
 
-let students = [
-        {name: "harry", subject: "JavaScript"},
-        {name: "Rohan", subject: "Machine Learning"}
-     ]
+let enstudents = [
+    {name: "harry", subject: "JavaScript"},
+    {name: "Rohan", subject: "Machine Learning"}
+ ]
 
 
 
-     function enrollStudent(student) {
-         return new Promise(function(resolve,reject) {
-             
-              setTimeout(() => {
-                   students.push(student) 
-                  console.log("student has been enrolled")
-                  const err=true
-                  if (!err) {
-                      resolve()
-                  }
-
-                  else{
-                reject()
-                  }
-                }, 5000);
-           
-    })
+ function enrollStudent(student) {
+     return new Promise(function(resolve,reject) {
          
-     }
- 
-     function getstudents() {
-         setTimeout(() => {
-             let str="";
-            students.forEach(function(student) {
-                str+= `<li>${student.name}</li>`
-                
-            });
-            document.getElementById("students").innerHTML=str;
-            console.log("student has benn fetched")
-            }, 2000);
-         
-     }
-         let newstudent = {name: "Sourabh", subject: "Learning"}    
+          setTimeout(() => {
+               enstudents.push(student) 
+              console.log("student has been enrolled")
+              const err=true
+              if (!err) {
+                  resolve()
+              }
+
+              else{
+            reject()
+              }
+            }, 5000);
+       
+})
+     
+ }
+
+ function getstudents() {
+     setTimeout(() => {
+         let str="";
+        enstudents.forEach(function(student) {
+            str+= `<li>${student.name}</li>`
+            
+        });
+        document.getElementById("students").innerHTML=str;
+        console.log("student has benn fetched")
+        }, 2000);
+     
+ }
+     let newstudent = {name: "Sourabh", subject: "Learning"}    
 
 enrollStudent(newstudent).then(getstudents).catch(function() {console.log("some error occur")
-    
+
 })
 // getstudents()
+
+
+
