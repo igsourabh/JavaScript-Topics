daa1f7ad98546dca1294e610cad7d03bd12f62e3
IGSourabh <90496971+IGSourabh@users.noreply.github.com>
2021-11-11 11:38:43 +0530

    Update #48 regular expreion charater set.js

--- #48 regular expreion charater set.js
+++ #48 regular expreion charater set.js
@@ -1,1 +1,23 @@
 
+let reg=/harry/i;
+reg=/h[a-zA-z]rry/ //can be any from a to z//
+reg=/h[aty]rry is [sb]ood [s]rogrammer/ //can be an a,t or y//
+reg=/h[^aty]rry/ //can not be only at a, t , y//
+reg=/h[a-zA-z]rry[0-9]/ //can be any k senstive or number//
+
+let str="harry9 is good programmer"
+
+let result=reg.exec(str)
+
+console.log(result)
+
+if (reg.test(str)) {
+
+    console.log(` ${str} match the exprection ${reg.source} `)
+    
+}
+
+else{   
+     console.log(`${str} not match the exprection ${reg.source}`)
+
+}
