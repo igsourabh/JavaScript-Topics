c66add7c9269c04c30204fa5071144cf8458472b
IGSourabh <90496971+IGSourabh@users.noreply.github.com>
2021-11-26 12:46:40 +0530

    Create #58JavaScript Sets.js

---
+++ #58JavaScript Sets.js
@@ -0,0 +1,19 @@
+
+let myset= new Set();
+myset.add('this');
+myset.add('this2');
+myset.add('setiing');
+myset.add(45);
+myset.add(45);
+myset.add(1515);
+
+console.log("this my",myset)
+
+
+// has use for avilablity if  avilable they sat true and false
+console.log(myset.has(45))
+
+// myset.delete(45)
+console.log("this my",myset)
+
+
