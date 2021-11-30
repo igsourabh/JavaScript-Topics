3bfb3098a3db77986c2733bc97f95d9c3104f4b6
IGSourabh <90496971+IGSourabh@users.noreply.github.com>
2021-11-30 19:02:02 +0530

    Update #35Ajax tutorial.js

--- #35Ajax tutorial.js
+++ #35Ajax tutorial.js
@@ -25,3 +25,29 @@ xhr.onload=function () {
 xhr.send()
 }
 
+// for pot request
+let fetchbtn=document.getElementById("fetchbtn")
+fetchbtn.addEventListener("click",buttonclickhandler)
+
+function buttonclickhandler() {
+    
+const xhr= new XMLHttpRequest()
+// xhr.open('GET','https://jsonplaceholder.typicode.com/todos/1',true)
+xhr.open('POST','https://dummy.restapiexample.com/api/v1/create',true)
+xhr.getResponseHeader('Content-type', 'application/json')
+
+xhr.onprogress=function() {
+    console.log("on progress")
+    }
+xhr.onload=function () {
+    if (this.status===200) {
+        
+        console.log(this.responseText)
+    }
+    else{
+        console.log("some error occured")
+    }
+}
+params={"name":"test121","salary":"123","age":"23"}
+xhr.send(params)
+}
