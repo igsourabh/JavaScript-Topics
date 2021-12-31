1557eaabde25f56b7749328cd00aac65adb092ea
igsourabh <90496971+igsourabh@users.noreply.github.com>
2021-12-31 10:46:01 +0530

    Update java script dsa #1.js

--- java script dsa #1.js
+++ java script dsa #1.js
@@ -16,3 +16,25 @@ function getpairofArray(array) {
   console.log(result);
   
 //   in  this aloithim time complexity is quardic
+
+
+function getpairofArray(array) {
+  let left = 0;
+  let right = array.length - 1;
+
+  while (left < right) {
+    sum = array[left] + array[right];
+    if (sum === 0) {
+      return [array[left], array[right]];
+    } else if (sum > 0) {
+      right--;
+    } else {
+      left++;
+    }
+  }
+}
+
+let solve = getpairofArray([-5, -4, -3, -2, 0, 2, 4, 6, 8]);
+console.log(solve);
+
+// this is leniear
